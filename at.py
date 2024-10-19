09aa84389608a8efca6ccdacfd3ba0fa9182f28f
Annu-Alex <annualex210@gmail.com>
2024-10-19 15:53:57 +0530

    Add files via upload

---
+++ at.py
@@ -0,0 +1,7 @@
+first_name = input("Enter your first name")
+last_name  = input("Enter your last name")
+name = first_name+" "+last_name
+print(name)
+first_name_length = len(first_name)
+extracted = [first_name_length+1]
+print([extracted])
